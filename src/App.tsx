1d6ac6e4a25db4b2250def5ebb85ff2cf2578fca
Wojciech Grochecki <wojciech5522@gmail.com>
2023-07-19 14:24:23 +0200

    add attribution

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,22 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-cream px-3 py-5">
+    <div className="flex h-screen flex-col items-center justify-center gap-y-5 bg-cream px-3 py-5">
       <ProductPreviewCard />
+      <div className="text-primary-text-very-dark-desaturated-blue desktop:text-lg w-[90%] text-center">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          className=" whitespace-nowrap text-darker-cyan hover:text-dark-cyan"
+          href="#"
+        >
+          Wojciech Grochecki
+        </a>
+        .
+      </div>
     </div>
   );
 }
